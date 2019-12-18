bb279e0662ab9099124fcf6858feaac834d11a30
Gia Edgington <gia.edgington@gmail.com>
2019-12-18 10:22:30 -0500

    yelp api search request, cors required

--- restaurants.js
+++ restaurants.js
@@ -8,5 +8,34 @@ require('dotenv/config');
 const apiKey = process.env.API_KEY;
 const client = yelp.client(apiKey);
 
+router.get('/', (req, res) => {
+
+    let searchRequest = {};
+
+    //Determine if request is location or coordinate based search and set parameters.
+
+    if (req.query.location){
+        searchRequest = {
+            term: 'Restaurants',
+            location: req.query.location
+        };
+    } else if (req.query.latitute && req.query.longitude) {
+        searchRequest = {
+            term: 'Restaurants',
+            latitude: req.query.latitude,
+            longitude: req.query.longitude
+        };
+    }
+
+    //Submit Yelp API search with request.
+
+    client.search(searchRequest).then(response => {
+        const responseJson = JSON.stringify(response.jsonBody.businesses, null, 4);
+        res.send(responseJson);
+    }).catch(e => {
+        res.send(e);
+    });
+});
+
 
 module.exports = router;
